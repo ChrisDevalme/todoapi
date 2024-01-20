00d88c33e5fe3e75b3c3e64d25703fcd1c7533fd
Chris Devalme <cdevalme@gmail.com>
2024-01-20 10:58:05 -0500

    Added Model & Routes for Todo

---
+++ models/todo.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const todoSchema = new mongoose.Schema({
+    title: { type: String, require: true },
+    description: { type: String, require: true },
+    completed: { type: Boolean, require: true }
+}, {
+    timestamps: true
+})
+
+const Todo = mongoose.model("Todo", todoSchema)
+
+module.exports = Todo
