4170d9013ffe3b53107c8f68f10fd56c0177c254
Chris Devalme <cdevalme@gmail.com>
2024-01-20 12:51:57 -0500

    Added destroy and Update Controllers

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -10,6 +10,7 @@ exports.index = async function index(req,res) {
         res.status(400).json({ msg: error.message })
     }
 }
+
 exports.create = async function create(req,res) {
     // Make a new todo in database
     try {
@@ -23,7 +24,9 @@ exports.create = async function create(req,res) {
 exports.update = async function update(req,res) {
     // Update exsiting todo in database
     try {
-        
+        const updatedTodo = await Todo.findOneAndUpdate({ _id: req.params.id}, req.body, { new: true }) 
+        // ({what were updating}, what we want to update it to, {makes it return the new todo rather than the old one})
+        res.status(200).json(updatedTodo)
     } catch (error) {
         res.status(400).json({ msg: error.message })
     }
@@ -32,7 +35,8 @@ exports.update = async function update(req,res) {
 exports.destroy = async function destroy(req,res) {
     // Delete an exsisting todo in database
     try {
-        
+        const deletedTodo = await Todo.findOneAndDelete({ _id: req.params.id })
+        res.status(200).json({ msg: `The todo with the Id of ${deletedTodo._id} was deleted from the MonogoDb database, no further action necessary.` })
     } catch (error) {
         res.status(400).json({ msg: error.message })
     }
@@ -41,7 +45,7 @@ exports.destroy = async function destroy(req,res) {
 exports.show = async function show(req,res) {
     // Show 1 individual todo
     try {
-        
+        const foundTodo = await Todo.findById({ _id: req.params.id})
     } catch (error) {
         res.status(400).json({ msg: error.message })
     }
